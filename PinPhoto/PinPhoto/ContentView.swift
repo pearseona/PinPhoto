3edb79135d94c53945297d673897436c8016d1af
배선아 <baeseona@baeseon-aui-Mac.local>
2026-05-25 03:37:59 -0700

    feat: Xcode 프로젝트 초기 세팅 및 구조 설정

---
+++ PinPhoto/PinPhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinPhoto
+//
+//  Created by 배선아 on 2026/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinPhoto/PinPhoto/PinPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinPhotoApp.swift
+//  PinPhoto
+//
+//  Created by 배선아 on 2026/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PinPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
